90b58fc22b75602832160d5e32ecf03a3274319d
mcy03 <manuelcaler2003@gmail.com>
2024-05-22 09:09:47 +0200

    servicios it

--- view/serviciosIT.php
+++ view/serviciosIT.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style/assets.css">
-    <link rel="stylesheet" type="text/css" href="./style/esqueletoServiciosLibreria.css?1.0">
     <link rel="stylesheet" type="text/css" href="./style/serviciosIT.css?1.0">
     <title>Servicios IT Miratelecomunicacions</title>
 </head>
@@ -14,26 +13,46 @@
             <h1>Servicios IT</h1>
         </div>
 
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Quisque a risus non sapien facilisis egestas. 
-            Vivamus scelerisque ex ipsum, a tempor velit tristique id. 
-            Ut in placerat erat.</span>
+        <span>Su aliado estratégico</span>
     </section>
     <section class="ubicacion">
         <div class="ruta-ubi">
             <a class="ruta-page" href="<?=url?>">Home / </a><p class="page">Servicios IT</p>
         </div>
     </section>
-    <section class="content section-one">
-        <div></div>
-    </section>
-    <section class="content section-two">
-    <div></div>
-    </section>
-    <section class="content section-three">
-        <article><div></div></article>
-        <article><div></div></article>
-        <article><div></div></article>
-    </section>
+    <div class="content">
+        <section class="intro-txt">
+            <p>
+                Somos un partner tecnológico de extremo a extremo. 
+                La nutrida combinación de ser un partner de capacitación y de servicios profesionales en apoyo al partner, 
+                nos convierte en el aliado estratégico que cualquier partner Cisco necesita para llevar a cabo sus proyectos.
+            </p>
+        </section>
+        <section class="compromiso">
+            <h3 class="title-section">NUESTRO COMPROMISO</h3>
+            <div id="img_compromiso"></div>
+        </section>
+        <section class="fases">
+            <div class="txt-fases">
+                <h3 class="title-section">FASES DEL PROYECTO</h3>
+                <ul>
+                    <li>Consultoría Estratégica</li>
+                    <p>
+                        Nuestro equipo de expertos podrá ayudarle a tomar la decisión, 
+                        analizar la viabilidad e innovar acorde a la estrategia de cada compañía, 
+                        con el objetivo de transformar el negocio creando un valor diferencial.
+                    </p>
+                    <li>Consultoría tecnológica</li>
+                    <p>
+                        Le ayudará al partner a determinar y le asesorará en la estrategia adecuada para la arquitectura de red 
+                        y la traducción de la estrategia técnica que requiere su cliente en una solución real de Cisco.
+                    </p>
+                    <li>Retorno de la inversión</li>
+                    <li>Soporte</li>
+                </ul>
+            </div>
+        </section>
+    </div>
+    
 </body>
 </html>
